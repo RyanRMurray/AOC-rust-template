3a44d4b22b887ac5f8efd0c8ee66325e9fadc8c7
Ryan Murray <RyanRMurray@Hotmail.co.uk>
2023-11-27 21:02:16 +0000

    delete example files

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -1,11 +1,8 @@
-mod day01;
-mod day18;
 pub mod templates;
 
 use anyhow::Result;
 
 /// Add new solutions to this const
-pub const SOLUTIONS: [fn(&str) -> Result<f32>; 2] = [
-	day01::day01,
-	day18::day18,
+pub const SOLUTIONS: [fn(&str) -> Result<f32>; 0] = [
+
 ];
